8884cdc5b58fece664d24d7c7a51a66383f98815
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-10-16 18:39:17 +1100

    test to ensure we dont return macosx files

--- __tests__/zipizape.spec.ts
+++ __tests__/zipizape.spec.ts
@@ -1,31 +1,44 @@
-jest.mock('jszip', () => {
-  return {
-    loadAsync() {
-      return {
-        forEach(callback) {
-          callback('file_name.png')
-          callback('video.mp4')
-        }
-      }
-    }
-  }
-});
-
+jest.mock('jszip')
+import * as Jszip from 'jszip';
 import { ZipiZape } from "../src";
 
 describe('zipizape', () => {
-  const setup = () => {
-    return {
-      
-    };
-  };
-  
-  it('should return all the entries from the zip', async () => {
+  const setup = async (mockValue?: any) => {
+    const defaultMockValue = {
+      forEach(callback) {
+        callback('file_name.png')
+        callback('video.mp4')
+      }
+    }
+
+    jest.spyOn(Jszip, 'loadAsync').mockReturnValue(mockValue || defaultMockValue)
+    
     const zipi = new ZipiZape();
     const file = new File([new Blob()], '');
     const entries = await zipi.readFile(file);
 
+    return {
+      entries
+    };
+  };
+  
+  it('should return all the entries from the zip', async () => {
+    const {entries} = await setup();
+
     expect(entries).toHaveLength(2)
     expect(entries[0].name).toEqual('file_name.png');
   });
+
+  it('should ignore macox entries', async () => {
+    const {entries} = await setup({
+      forEach(callback) {
+        callback('__MACOSX/foo.png')
+        callback('__MACOSX');
+        callback('video.mp4')
+      }
+    });
+    
+    expect(entries).toHaveLength(1)
+    expect(entries[0].name).toEqual('video.mp4');
+  })
 });
